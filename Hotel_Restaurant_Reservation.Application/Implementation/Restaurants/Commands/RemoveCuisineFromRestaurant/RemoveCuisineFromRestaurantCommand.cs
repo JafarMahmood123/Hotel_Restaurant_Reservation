4d527717c3f0aa9742023a7c0282391e4a6baf7c
JafarMahmood123 <gafarARC.com@gmail.com>
2025-05-11 21:39:59 +0300

    Add RemoveCuisineFromRestaurant command

---
+++ Hotel_Restaurant_Reservation.Application/Implementation/Restaurants/Commands/RemoveCuisineFromRestaurant/RemoveCuisineFromRestaurantCommand.cs
@@ -0,0 +1,16 @@
+﻿using Hotel_Restaurant_Reservation.Application.Abstractions.Messaging;
+using Hotel_Restaurant_Reservation.Domain.Entities;
+
+namespace Hotel_Restaurant_Reservation.Application.Implementation.Restaurants.Commands.RemoveCuisineFromRestaurant;
+
+public class RemoveCuisineFromRestaurantCommand : ICommand<Cuisine>
+{
+    public RemoveCuisineFromRestaurantCommand(Guid restaurantId, Guid cuisineId)
+    {
+        RestaurantId = restaurantId;
+        CuisineId = cuisineId;
+    }
+
+    public Guid RestaurantId { get; }
+    public Guid CuisineId { get; }
+}
---
+++ Hotel_Restaurant_Reservation.Application/Implementation/Restaurants/Commands/RemoveCuisineFromRestaurant/RemoveCuisineFromRestaurantCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using Hotel_Restaurant_Reservation.Application.Abstractions.Messaging;
+using Hotel_Restaurant_Reservation.Domain.Abstractions;
+using Hotel_Restaurant_Reservation.Domain.Entities;
+
+namespace Hotel_Restaurant_Reservation.Application.Implementation.Restaurants.Commands.RemoveCuisineFromRestaurant;
+
+public class RemoveCuisineFromRestaurantCommandHandler : ICommandHandler<RemoveCuisineFromRestaurantCommand, Cuisine>
+{
+    private readonly IGenericRepository<RestaurantCuisine> restaurantCuisineRepository;
+    private readonly IGenericRepository<Cuisine> cuisineRepository;
+
+    public RemoveCuisineFromRestaurantCommandHandler(IGenericRepository<RestaurantCuisine> restaurantCuisineRepository,
+        IGenericRepository<Cuisine> cuisineRepository)
+    {
+        this.restaurantCuisineRepository = restaurantCuisineRepository;
+        this.cuisineRepository = cuisineRepository;
+    }
+
+    public async Task<Cuisine> Handle(RemoveCuisineFromRestaurantCommand request, CancellationToken cancellationToken)
+    {
+        var restaurantId = request.RestaurantId;
+        var cuisineId = request.CuisineId;
+
+        var restaurantCuisine = await restaurantCuisineRepository.GetFirstOrDefaultAsync(x => x.RestaurantId == restaurantId
+        && x.CuisineId == cuisineId);
+
+        restaurantCuisineRepository.Remove(restaurantCuisine);
+
+        var cuisine = await cuisineRepository.GetByIdAsync(cuisineId);
+
+        return cuisine;
+    }
+}
