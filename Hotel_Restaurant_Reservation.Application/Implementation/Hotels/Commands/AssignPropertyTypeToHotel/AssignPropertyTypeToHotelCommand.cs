9cd5bfb4d6ea182cf9e55bbcbb913a65510a51f5
Jafar Mahmood <Jafar Mahmood@DESKTOP-TVPDTBP>
2025-07-11 23:10:08 +0300

    Add AssignPropertyTypeToHotel

---
+++ Hotel_Restaurant_Reservation.Application/Implementation/Hotels/Commands/AssignPropertyTypeToHotel/AssignPropertyTypeToHotelCommand.cs
@@ -0,0 +1,18 @@
+﻿using Hotel_Restaurant_Reservation.Application.Abstractions.Messaging;
+using Hotel_Restaurant_Reservation.Application.Implementation.PropertyTypes.Queries;
+using Hotel_Restaurant_Reservation.Domain.Entities;
+using Hotel_Restaurant_Reservation.Domain.Shared;
+
+namespace Hotel_Restaurant_Reservation.Application.Implementation.Hotels.Commands.AssignPropertyTypeToHotel;
+
+public class AssignPropertyTypeToHotelCommand : ICommand<Result<PropertyTypeResponse>>
+{
+    public AssignPropertyTypeToHotelCommand(Guid hotelId, Guid propertyTypeId)
+    {
+        HotelId = hotelId;
+        PropertyTypeId = propertyTypeId;
+    }
+
+    public Guid HotelId { get; }
+    public Guid PropertyTypeId { get; }
+}
---
+++ Hotel_Restaurant_Reservation.Application/Implementation/Hotels/Commands/AssignPropertyTypeToHotel/AssignPropertyTypeToHotelCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using Hotel_Restaurant_Reservation.Application.Abstractions.Messaging;
+using Hotel_Restaurant_Reservation.Application.Implementation.PropertyTypes.Queries;
+using Hotel_Restaurant_Reservation.Domain.Abstractions;
+using Hotel_Restaurant_Reservation.Domain.Entities;
+using Hotel_Restaurant_Reservation.Domain.Shared;
+
+namespace Hotel_Restaurant_Reservation.Application.Implementation.Hotels.Commands.AssignPropertyTypeToHotel;
+
+public class AssignPropertyTypeToHotelCommandHandler : ICommandHandler<AssignPropertyTypeToHotelCommand, Result<PropertyTypeResponse>>
+{
+    private readonly IGenericRepository<Hotel> _hotelRepository;
+    private readonly IGenericRepository<PropertyType> _propertyTypeRepository;
+    private readonly IMapper _mapper;
+
+    public AssignPropertyTypeToHotelCommandHandler(IGenericRepository<Hotel> hotelRepository,
+        IGenericRepository<PropertyType> propertyTypeRepository, IMapper mapper)
+    {
+        _hotelRepository = hotelRepository;
+        _propertyTypeRepository = propertyTypeRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<Result<PropertyTypeResponse>> Handle(AssignPropertyTypeToHotelCommand request, CancellationToken cancellationToken)
+    {
+        var hotel = await _hotelRepository.GetByIdAsync(request.HotelId);
+        if (hotel is null)
+        {
+            return Result.Failure<PropertyTypeResponse>(DomainErrors.Hotel.NotFound(request.HotelId));
+        }
+
+        var propertyType = await _propertyTypeRepository.GetByIdAsync(request.PropertyTypeId);
+        if (propertyType is null)
+        {
+            return Result.Failure<PropertyTypeResponse>(DomainErrors.PropertyType.NotFound(request.PropertyTypeId));
+        }
+
+        hotel.PropertyTypeId = request.PropertyTypeId;
+
+        await _hotelRepository.UpdateAsync(request.HotelId, hotel);
+        await _hotelRepository.SaveChangesAsync();
+
+
+        var response = _mapper.Map<PropertyTypeResponse>(propertyType);
+
+        return Result.Success(response);
+    }
+}
