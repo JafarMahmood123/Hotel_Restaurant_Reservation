268d40de60306949487f7dfeccbe46f6651bebc3
Jafar Mahmood <Jafar Mahmood@DESKTOP-TVPDTBP>
2025-07-14 21:13:35 +0300

    Add error handling to GetAllCities

--- Hotel_Restaurant_Reservation.Application/Implementation/Cities/Queries/GetAllCities/GetAllCitiesQuery.cs
+++ Hotel_Restaurant_Reservation.Application/Implementation/Cities/Queries/GetAllCities/GetAllCitiesQuery.cs
@@ -1,8 +1,9 @@
 ﻿using Hotel_Restaurant_Reservation.Application.Abstractions.Messaging;
-using Hotel_Restaurant_Reservation.Domain.Entities;
+using Hotel_Restaurant_Reservation.Application.Implementation.Cities.Queries;
+using Hotel_Restaurant_Reservation.Domain.Shared;
 
 namespace Hotel_Restaurant_Reservation.Application.Implementation.Cities.Queries.GetAllCities;
 
-public class GetAllCitiesQuery : IQuery<IEnumerable<City>?>
+public class GetAllCitiesQuery : IQuery<Result<IEnumerable<CityResponse>>>
 {
 }
--- Hotel_Restaurant_Reservation.Application/Implementation/Cities/Queries/GetAllCities/GetAllCitiesQueryHandler.cs
+++ Hotel_Restaurant_Reservation.Application/Implementation/Cities/Queries/GetAllCities/GetAllCitiesQueryHandler.cs
@@ -1,20 +1,27 @@
-﻿using Hotel_Restaurant_Reservation.Application.Abstractions.Messaging;
+﻿using AutoMapper;
+using Hotel_Restaurant_Reservation.Application.Abstractions.Messaging;
 using Hotel_Restaurant_Reservation.Application.Abstractions.Repositories;
+using Hotel_Restaurant_Reservation.Application.Implementation.Cities.Queries;
 using Hotel_Restaurant_Reservation.Domain.Entities;
+using Hotel_Restaurant_Reservation.Domain.Shared;
 
 namespace Hotel_Restaurant_Reservation.Application.Implementation.Cities.Queries.GetAllCities;
 
-public class GetAllCitiesQueryHandler : IQueryHandler<GetAllCitiesQuery, IEnumerable<City>?>
+public class GetAllCitiesQueryHandler : IQueryHandler<GetAllCitiesQuery, Result<IEnumerable<CityResponse>>>
 {
-    private readonly IGenericRepository<City> genericRepository;
+    private readonly IGenericRepository<City> _cityRepository;
+    private readonly IMapper _mapper;
 
-    public GetAllCitiesQueryHandler(IGenericRepository<City> genericRepository)
+    public GetAllCitiesQueryHandler(IGenericRepository<City> cityRepository, IMapper mapper)
     {
-        this.genericRepository = genericRepository;
+        _cityRepository = cityRepository;
+        _mapper = mapper;
     }
 
-    public async Task<IEnumerable<City>?> Handle(GetAllCitiesQuery request, CancellationToken cancellationToken)
+    public async Task<Result<IEnumerable<CityResponse>>> Handle(GetAllCitiesQuery request, CancellationToken cancellationToken)
     {
-        return await genericRepository.GetAllAsync();
+        var cities = await _cityRepository.GetAllAsync();
+        var cityResponses = _mapper.Map<IEnumerable<CityResponse>>(cities);
+        return Result.Success(cityResponses);
     }
 }
