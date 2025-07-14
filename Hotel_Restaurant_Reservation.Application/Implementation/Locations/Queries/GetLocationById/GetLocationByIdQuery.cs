1d8bfefac233998256e2a8d4e7490012b73cf65a
Jafar Mahmood <Jafar Mahmood@DESKTOP-TVPDTBP>
2025-07-14 22:29:01 +0300

    Add error handling to GetLocationById

--- Hotel_Restaurant_Reservation.Application/Implementation/Locations/Queries/GetLocationById/GetLocationByIdQuery.cs
+++ Hotel_Restaurant_Reservation.Application/Implementation/Locations/Queries/GetLocationById/GetLocationByIdQuery.cs
@@ -1,9 +1,10 @@
 ﻿using Hotel_Restaurant_Reservation.Application.Abstractions.Messaging;
-using Hotel_Restaurant_Reservation.Domain.Entities;
+using Hotel_Restaurant_Reservation.Application.Implementation.Locations.Queries;
+using Hotel_Restaurant_Reservation.Domain.Shared;
 
 namespace Hotel_Restaurant_Reservation.Application.Implementation.Locations.Queries.GetLocationById;
 
-public class GetLocationByIdQuery : IQuery<Location?>
+public class GetLocationByIdQuery : IQuery<Result<LocationResponse>>
 {
     public GetLocationByIdQuery(Guid id)
     {
--- Hotel_Restaurant_Reservation.Application/Implementation/Locations/Queries/GetLocationById/GetLocationByIdQueryHandler.cs
+++ Hotel_Restaurant_Reservation.Application/Implementation/Locations/Queries/GetLocationById/GetLocationByIdQueryHandler.cs
@@ -1,20 +1,33 @@
-﻿using Hotel_Restaurant_Reservation.Application.Abstractions.Messaging;
+﻿using AutoMapper;
+using Hotel_Restaurant_Reservation.Application.Abstractions.Messaging;
 using Hotel_Restaurant_Reservation.Application.Abstractions.Repositories;
+using Hotel_Restaurant_Reservation.Application.Implementation.Locations.Queries;
 using Hotel_Restaurant_Reservation.Domain.Entities;
+using Hotel_Restaurant_Reservation.Domain.Shared;
 
 namespace Hotel_Restaurant_Reservation.Application.Implementation.Locations.Queries.GetLocationById;
 
-public class GetLocationByIdQueryHandler : IQueryHandler<GetLocationByIdQuery, Location?>
+public class GetLocationByIdQueryHandler : IQueryHandler<GetLocationByIdQuery, Result<LocationResponse>>
 {
-    private readonly IGenericRepository<Location> genericRepository;
+    private readonly IGenericRepository<Location> _locationRepository;
+    private readonly IMapper _mapper;
 
-    public GetLocationByIdQueryHandler(IGenericRepository<Location> genericRepository)
+    public GetLocationByIdQueryHandler(IGenericRepository<Location> locationRepository, IMapper mapper)
     {
-        this.genericRepository = genericRepository;
+        _locationRepository = locationRepository;
+        _mapper = mapper;
     }
 
-    public async Task<Location?> Handle(GetLocationByIdQuery request, CancellationToken cancellationToken)
+    public async Task<Result<LocationResponse>> Handle(GetLocationByIdQuery request, CancellationToken cancellationToken)
     {
-        return await genericRepository.GetByIdAsync(request.Id);
+        var location = await _locationRepository.GetByIdAsync(request.Id);
+
+        if (location is null)
+        {
+            return Result.Failure<LocationResponse>(DomainErrors.Location.NotFound(request.Id));
+        }
+
+        var locationResponse = _mapper.Map<LocationResponse>(location);
+        return Result.Success(locationResponse);
     }
 }
