21d0a3c9352a4f138b4c2a9ac5cc8903ec530fcd
Jafar Mahmood <Jafar Mahmood@DESKTOP-TVPDTBP>
2025-07-14 13:41:19 +0300

    Implement GetBookingDishesByBookingId

---
+++ Hotel_Restaurant_Reservation.Application/Implementation/BookingDishes/Queries/GetBookingDishesByBookingId/GetBookingDishesByBookingIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using Hotel_Restaurant_Reservation.Application.Abstractions.Messaging;
+using Hotel_Restaurant_Reservation.Application.Implementation.BookingDishes.Queries;
+using Hotel_Restaurant_Reservation.Domain.Shared;
+
+namespace Hotel_Restaurant_Reservation.Application.Implementation.BookingDishes.Queries.GetBookingDishesByBookingId;
+
+public class GetBookingDishesByBookingIdQuery : IQuery<Result<IEnumerable<BookingDishResponse>>>
+{
+    public GetBookingDishesByBookingIdQuery(Guid bookingId)
+    {
+        BookingId = bookingId;
+    }
+
+    public Guid BookingId { get; }
+}
---
+++ Hotel_Restaurant_Reservation.Application/Implementation/BookingDishes/Queries/GetBookingDishesByBookingId/GetBookingDishesByBookingIdQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Hotel_Restaurant_Reservation.Application.Abstractions.Messaging;
+using Hotel_Restaurant_Reservation.Application.Abstractions.Repositories;
+using Hotel_Restaurant_Reservation.Domain.Entities;
+using Hotel_Restaurant_Reservation.Domain.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hotel_Restaurant_Reservation.Application.Implementation.BookingDishes.Queries.GetBookingDishesByBookingId;
+
+public class GetBookingDishesByBookingIdQueryHandler : IQueryHandler<GetBookingDishesByBookingIdQuery, Result<IEnumerable<BookingDishResponse>>>
+{
+    private readonly IGenericRepository<BookingDish> _bookingDishRepository;
+    private readonly IMapper _mapper;
+
+    public GetBookingDishesByBookingIdQueryHandler(IGenericRepository<BookingDish> bookingDishRepository, IMapper mapper)
+    {
+        _bookingDishRepository = bookingDishRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<Result<IEnumerable<BookingDishResponse>>> Handle(GetBookingDishesByBookingIdQuery request, CancellationToken cancellationToken)
+    {
+        var bookingDishes = await _bookingDishRepository
+            .Where(bd => bd.RestaurantBookingId == request.BookingId)
+            .ToListAsync(cancellationToken);
+
+        var bookingDishResponses = _mapper.Map<IEnumerable<BookingDishResponse>>(bookingDishes);
+
+        return Result.Success(bookingDishResponses);
+    }
+}
