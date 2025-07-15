182460b1655f5736e9f22104ebaa47f1a19dbe6f
Jafar Mahmood <Jafar Mahmood@DESKTOP-TVPDTBP>
2025-07-15 15:40:58 +0300

    Implement GetTagsByRestaurantId

---
+++ Hotel_Restaurant_Reservation.Application/Implementation/Tags/Queries/GetTagsByRestaurantId/GetTagsByRestaurantIdQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Hotel_Restaurant_Reservation.Application.Abstractions.Messaging;
+using Hotel_Restaurant_Reservation.Application.Abstractions.Repositories;
+using Hotel_Restaurant_Reservation.Domain.Entities;
+using Hotel_Restaurant_Reservation.Domain.Shared;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Hotel_Restaurant_Reservation.Application.Implementation.Tags.Queries.GetTagsByRestaurantId
+{
+    public class GetTagsByRestaurantIdQueryHandler : IQueryHandler<GetTagsByRestaurantIdQuery, Result<IEnumerable<TagResponse>>>
+    {
+        private readonly IGenericRepository<RestaurantTag> _restaurantTagRepository;
+        private readonly IMapper _mapper;
+
+        public GetTagsByRestaurantIdQueryHandler(IGenericRepository<RestaurantTag> restaurantTagRepository, IMapper mapper)
+        {
+            _restaurantTagRepository = restaurantTagRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Result<IEnumerable<TagResponse>>> Handle(GetTagsByRestaurantIdQuery request, CancellationToken cancellationToken)
+        {
+            var restaurantTags = await _restaurantTagRepository
+                .Where(rt => rt.RestaurantId == request.RestaurantId)
+                .Include(rt => rt.Tag)
+                .ToListAsync(cancellationToken);
+
+            var tags = restaurantTags.Select(rt => rt.Tag);
+
+            var tagResponses = _mapper.Map<IEnumerable<TagResponse>>(tags);
+
+            return Result.Success(tagResponses);
+        }
+    }
+}
--- Hotel_Restaurant_Reservation.Presentation/Controllers/TagsController.cs
+++ Hotel_Restaurant_Reservation.Presentation/Controllers/TagsController.cs
@@ -1,6 +1,7 @@
 ﻿using Hotel_Restaurant_Reservation.Application.DTOs.TagDTOs;
 using Hotel_Restaurant_Reservation.Application.Implementation.Tags.Commands.AddTag;
 using Hotel_Restaurant_Reservation.Application.Implementation.Tags.Queries.GetAllTags;
+using Hotel_Restaurant_Reservation.Application.Implementation.Tags.Queries.GetTagsByRestaurantId;
 using Hotel_Restaurant_Reservation.Presentation.Abstractions;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -30,6 +31,18 @@ namespace Hotel_Restaurant_Reservation.Presentation.Controllers
         public async Task<IActionResult> GetAllTags(CancellationToken cancellationToken)
         {
             var query = new GetAllTagsQuery();
+            var result = await Sender.Send(query, cancellationToken);
+            if (result.IsFailure)
+            {
+                return NotFound(result.Error);
+            }
+            return Ok(result.Value);
+        }
+
+        [HttpGet("restaurant/{restaurantId:guid}")]
+        public async Task<IActionResult> GetTagsByRestaurantId(Guid restaurantId, CancellationToken cancellationToken)
+        {
+            var query = new GetTagsByRestaurantIdQuery(restaurantId);
             var result = await Sender.Send(query, cancellationToken);
             if (result.IsFailure)
             {
