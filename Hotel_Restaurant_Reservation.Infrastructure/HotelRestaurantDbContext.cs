551e47fe6d5f3a357df2efa4f4af3d12cf9b241c
Jafar Mahmood <Jafar Mahmood@DESKTOP-TVPDTBP>
2025-07-15 09:58:40 +0300

    Add more DbSets

--- Hotel_Restaurant_Reservation.Infrastructure/HotelRestaurantDbContext.cs
+++ Hotel_Restaurant_Reservation.Infrastructure/HotelRestaurantDbContext.cs
@@ -10,66 +10,44 @@ public class HotelRestaurantDbContext : DbContext
         
     }
     public DbSet<Hotel> Hotels { get; set; }
-
     public DbSet<Room> Rooms { get; set; }
-
     public DbSet<RoomAmenity> RoomAmenities { get; set; }
-
     public DbSet<RoomType> RoomTypes { get; set; }
-
     public DbSet<PropertyType> PropertyTypes { get; set; }
-
     public DbSet<CurrencyType> CurrencyTypes { get; set; }
-
     public DbSet<HotelReservation> HotelReservations { get; set; }
-
     public DbSet<RestaurantReview> RestaurantReviews { get; set; }
-
     public DbSet<Country> Countries { get; set; }
-
     public DbSet<City> Cities { get; set; }
-
     public DbSet<LocalLocation> LocalLocations { get; set; }
-
     public DbSet<CityLocalLocations> CityLocalLocations { get; set; }
-
     public DbSet<Location> Locations { get; set; }
-
     public DbSet<Restaurant> Restaurants { get; set; }
-
     public DbSet<RestaurantCuisine> RestaurantCuisines { get; set; }
-
     public DbSet<RestaurantFeature> RestaurantFeatures { get; set; }
-
     public DbSet<RestaurantMealType> RestaurantMealTypes { get; set; }
-
     public DbSet<RestaurantDishPrice> RestaurantDishPrices { get; set; }
-
     public DbSet<RestaurantTag> RestaurantTags { get; set; }
-
     public DbSet<RestaurantWorkTime> RestaurantWorkTimes { get; set; }
-
     public DbSet<RestaurantCurrencyType> RestaurantCurrencyTypes { get; set; }
-
     public DbSet<WorkTime> WorkTimes { get; set; }
-
     public DbSet<Feature> Features { get; set; }
-
     public DbSet<Tag> Tags { get; set; }
-
     public DbSet<Cuisine> Cuisines { get; set; }
-
     public DbSet<MealType> MealTypes { get; set; }
-
     public DbSet<Dish> Dishes { get; set; }
-
     public DbSet<User> Customers { get; set; }
-
     public DbSet<Amenity> Amenities { get; set; }
-
     public DbSet<Event> Events { get; set; }
-
     public DbSet<EventRegistration> EventRegistrations { get; set; }
-
     public DbSet<RestaurantBooking> RestaurantBookings { get; set; }
+    public DbSet<BookingDish> BookingDishes { get; set; }
+    public DbSet<EventManager> EventManagers { get; set; }
+    public DbSet<EventReview> EventReviews { get; set; }
+    public DbSet<HotelAmenityPrice> HotelAmenityPrices { get; set; }
+    public DbSet<HotelCurrencyType> HotelCurrencyTypes { get; set; }
+    public DbSet<HotelManager> HotelManagers { get; set; }
+    public DbSet<HotelReview> HotelReviews { get; set; }
+    public DbSet<RestaurantManager> RestaurantManagers { get; set; }
+    public DbSet<Role> Roles { get; set; }
 }
