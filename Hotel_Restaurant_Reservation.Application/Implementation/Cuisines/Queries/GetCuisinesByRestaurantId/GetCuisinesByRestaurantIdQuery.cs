d6f640686a291f8cb4877860b932fb0c2b494f7d
Jafar Mahmood <Jafar Mahmood@DESKTOP-TVPDTBP>
2025-07-15 13:46:53 +0300

    Implement GetCuisinesByRestaurantId

---
+++ Hotel_Restaurant_Reservation.Application/Implementation/Cuisines/Queries/GetCuisinesByRestaurantId/GetCuisinesByRestaurantIdQuery.cs
@@ -0,0 +1,18 @@
+﻿using Hotel_Restaurant_Reservation.Application.Abstractions.Messaging;
+using Hotel_Restaurant_Reservation.Application.Implementation.Cuisines.Queries;
+using Hotel_Restaurant_Reservation.Domain.Shared;
+using System;
+using System.Collections.Generic;
+
+namespace Hotel_Restaurant_Reservation.Application.Implementation.Cuisines.Queries.GetCuisinesByRestaurantId
+{
+    public class GetCuisinesByRestaurantIdQuery : IQuery<Result<IEnumerable<CuisineResponse>>>
+    {
+        public GetCuisinesByRestaurantIdQuery(Guid restaurantId)
+        {
+            RestaurantId = restaurantId;
+        }
+
+        public Guid RestaurantId { get; }
+    }
+}
---
+++ Hotel_Restaurant_Reservation.Application/Implementation/Cuisines/Queries/GetCuisinesByRestaurantId/GetCuisinesByRestaurantIdQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Hotel_Restaurant_Reservation.Application.Abstractions.Messaging;
+using Hotel_Restaurant_Reservation.Application.Abstractions.Repositories;
+using Hotel_Restaurant_Reservation.Domain.Entities;
+using Hotel_Restaurant_Reservation.Domain.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hotel_Restaurant_Reservation.Application.Implementation.Cuisines.Queries.GetCuisinesByRestaurantId
+{
+    public class GetCuisinesByRestaurantIdQueryHandler : IQueryHandler<GetCuisinesByRestaurantIdQuery, Result<IEnumerable<CuisineResponse>>>
+    {
+        private readonly IGenericRepository<RestaurantCuisine> _restaurantCuisineRepository;
+        private readonly IMapper _mapper;
+
+        public GetCuisinesByRestaurantIdQueryHandler(IGenericRepository<RestaurantCuisine> restaurantCuisineRepository, IMapper mapper)
+        {
+            _restaurantCuisineRepository = restaurantCuisineRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Result<IEnumerable<CuisineResponse>>> Handle(GetCuisinesByRestaurantIdQuery request, CancellationToken cancellationToken)
+        {
+            var restaurantCuisines = await _restaurantCuisineRepository
+                .Where(rc => rc.RestaurantId == request.RestaurantId)
+                .Include(rc => rc.Cuisine)
+                .ToListAsync(cancellationToken);
+
+            var cuisines = restaurantCuisines.Select(rc => rc.Cuisine);
+
+            var cuisineResponses = _mapper.Map<IEnumerable<CuisineResponse>>(cuisines);
+
+            return Result.Success(cuisineResponses);
+        }
+    }
+}
