c78097d2615bba1320994d9c7fb6cc2c581124ec
Jafar Mahmood <Jafar Mahmood@DESKTOP-TVPDTBP>
2025-07-15 15:14:00 +0300

    Implement GetAllHotelReviewsByHotelId

---
+++ Hotel_Restaurant_Reservation.Application/Implementation/HotelReviews/Queries/GetAllHotelReviewsByHotelId/GetAllHotelReviewsByHotelIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using Hotel_Restaurant_Reservation.Application.Abstractions.Messaging;
+using Hotel_Restaurant_Reservation.Domain.Shared;
+
+namespace Hotel_Restaurant_Reservation.Application.Implementation.HotelReviews.Queries.GetAllHotelReviewsByHotelId
+{
+    public class GetAllHotelReviewsByHotelIdQuery : IQuery<Result<IEnumerable<HotelReviewResponse>>>
+    {
+        public GetAllHotelReviewsByHotelIdQuery(Guid hotelId)
+        {
+            HotelId = hotelId;
+        }
+
+        public Guid HotelId { get; }
+    }
+}
---
+++ Hotel_Restaurant_Reservation.Application/Implementation/HotelReviews/Queries/GetAllHotelReviewsByHotelId/GetAllHotelReviewsByHotelIdQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Hotel_Restaurant_Reservation.Application.Abstractions.Messaging;
+using Hotel_Restaurant_Reservation.Application.Abstractions.Repositories;
+using Hotel_Restaurant_Reservation.Domain.Entities;
+using Hotel_Restaurant_Reservation.Domain.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hotel_Restaurant_Reservation.Application.Implementation.HotelReviews.Queries.GetAllHotelReviewsByHotelId
+{
+    public class GetAllHotelReviewsByHotelIdQueryHandler : IQueryHandler<GetAllHotelReviewsByHotelIdQuery, Result<IEnumerable<HotelReviewResponse>>>
+    {
+        private readonly IGenericRepository<HotelReview> _hotelReviewRepository;
+        private readonly IMapper _mapper;
+
+        public GetAllHotelReviewsByHotelIdQueryHandler(IGenericRepository<HotelReview> hotelReviewRepository, IMapper mapper)
+        {
+            _hotelReviewRepository = hotelReviewRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Result<IEnumerable<HotelReviewResponse>>> Handle(GetAllHotelReviewsByHotelIdQuery request, CancellationToken cancellationToken)
+        {
+            var hotelReviews = await _hotelReviewRepository
+                .Where(hr => hr.HotelId == request.HotelId)
+                .ToListAsync(cancellationToken);
+
+            var hotelReviewResponses = _mapper.Map<IEnumerable<HotelReviewResponse>>(hotelReviews);
+
+            return Result.Success(hotelReviewResponses);
+        }
+    }
+}
